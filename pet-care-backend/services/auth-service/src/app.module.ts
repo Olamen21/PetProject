43ca5bd1b7f42c5c39c283c7d2f077807b1f0f74
ThanhThuy286 <thanhthuyhuynh756@gmail.com>
2026-04-21 16:29:04 +0700

    feat: add auth database and basic auth logic

--- pet-care-backend/services/auth-service/src/app.module.ts
+++ pet-care-backend/services/auth-service/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgresdb',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'pet_care_auth',
+      entities: [User],
+      synchronize: true,
+      retryAttempts: 10,
+      retryDelay: 3000,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
