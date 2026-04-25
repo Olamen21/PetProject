b6e153142980390c6ffad3aac503f7b6d88a894f
ThanhThuy286 <thanhthuyhuynh756@gmail.com>
2026-04-25 15:15:04 +0700

    roleSignup

--- pet-care-backend/services/auth-service/src/auth/dto/register.dto.ts
+++ pet-care-backend/services/auth-service/src/auth/dto/register.dto.ts
@@ -1,14 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
-  @ApiProperty({ example: 'thuy@example.com', description: 'Email dùng để đăng ký' })
+  @ApiProperty({
+    example: 'thuy@example.com',
+    description: 'Email dùng để đăng ký',
+  })
   email: string;
 
-  @ApiProperty({ example: '123456', description: 'Mật khẩu (tối thiểu 6 ký tự)' })
+  @ApiProperty({
+    example: '123456',
+    description: 'Mật khẩu (tối thiểu 6 ký tự)',
+  })
   password: string;
 
   @ApiProperty({ example: 'Huỳnh Thanh Thúy', description: 'Họ và tên' })
   full_name: string;
-  @ApiProperty({ example: 'VET_2026', description: 'Mã xác thực bác sĩ' })
-  adminKey: string;
+  @ApiProperty({ example: 'VET', description: 'Vai trò của người dùng' })
+  role: 'VET' | 'ADMIN' | 'USER';
 }
