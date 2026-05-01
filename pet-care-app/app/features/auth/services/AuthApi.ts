bcde41e0b8d936eda45db1114a757374773d1e54
Trần Thị Thanh Ngân <thanhngannt3110@gmail.com>
2026-05-01 22:31:18 +0700

    Token_AsyncStorage

--- pet-care-app/app/features/auth/services/AuthApi.ts
+++ pet-care-app/app/features/auth/services/AuthApi.ts
@@ -1,14 +1,22 @@
-import axios from "axios";
-
-const API_URL = process.env.EXPO_PUBLIC_API_URL_AUTH;
+import { removeToken, saveToken } from "./AuthStorage";
+import api from "./Api";
 
 export const signUp = async (data: { email: string; full_name: string; password: string }) => {
-  const res = await axios.post(`${API_URL}/auth/register`, data);
+  const res = await api.post(`/auth/register`, data);
   return res.data;
 };
 
 export const login = async (data: { email: string; password: string; full_name: string }) => {
-  const res = await axios.post(`${API_URL}/auth/login`, data);
+  const res = await api.post(`/auth/login`, data);
+
+   if (res.data?.token) {
+    await saveToken(res.data.token);
+  }
+
   return res.data;
 };
  
+
+export const logout = async () => {
+  await removeToken();
+};
