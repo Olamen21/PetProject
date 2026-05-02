abb588eb3f8210c50a38a29017b5fd4ae80ecc93
ThanhThuy286 <thanhthuyhuynh756@gmail.com>
2026-05-02 14:34:46 +0700

    fix api-gateway, docker, CORS

--- pet-care-backend/services/api-gateway/src/app.module.ts
+++ pet-care-backend/services/api-gateway/src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { RolesGuard } from './auth/guards/roles.guard';
+
 @Module({
   imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
--- pet-care-backend/services/api-gateway/src/auth/guards/roles.guard.ts
+++ pet-care-backend/services/api-gateway/src/auth/guards/roles.guard.ts
@@ -1,27 +1,29 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ROLES_KEY } from '../decorators/roles.decorator';
-import { Role } from '../role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
       context.getHandler(),
       context.getClass(),
     ]);
 
-    if (!requiredRoles) {
-      return true;
+    if (!requiredRoles) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (!user || !requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('Bạn không đủ quyền để truy cập trang này');
     }
 
-    const request = context
-      .switchToHttp()
-      .getRequest<{ user?: { role: Role } }>();
-    const user = request.user;
-
-    return !!(user && user.role && requiredRoles.includes(user.role));
+    return true;
   }
 }
