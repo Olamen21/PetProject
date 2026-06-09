04e7eb8b003461c32d700f6b68ea9f9c57e432a4
ThanhThuy286 <thanhthuyhuynh756@gmail.com>
2026-06-09 22:02:58 +0700

    init review-service

--- pet-care-backend/services/review-service/src/app.module.ts
+++ pet-care-backend/services/review-service/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReviewModule } from './review/review.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from './review/entities/review.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgresdb',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'pet_care_review',
+      entities: [Review],
+      synchronize: true,
+      retryAttempts: 10,
+      retryDelay: 3000,
+    }),
+    ReviewModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- pet-care-backend/services/review-service/src/main.ts
+++ pet-care-backend/services/review-service/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  const Config = new DocumentBuilder()
+    .setTitle('Pet Care - Medical Record Service')
+    .setDescription('Tài liệu API cho dịch vụ quản lý hồ sơ y tế')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Nhập Token của bạn vào đây',
+        in: 'header',
+      },
+      'token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, Config);
+  SwaggerModule.setup('api/docs', app, document);
+  await app.listen(3009);
 }
 bootstrap();
